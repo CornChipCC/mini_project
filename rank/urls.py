f0160678ce7d59690606a074e85690ba6ed363c1
ConChipCC <sgchfh@naver.com>
2022-01-20 19:49:21 +0900

    first commit

---
+++ rank/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+    path('rank1_100/', views.rank1_100),
+    path('insert/', views.insert),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
